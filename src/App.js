91ec70e7b669ee8b7485a962e9533511a5975b56
Rakesh-TN <tnrakesh68@outlook.com>
2023-10-20 18:50:02 +0530

    Task One Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="cards">
+        <div>
+          <div className="text-center">
+            <p className="fs-1">Congratulations</p>
+          </div>
+          <div className="bg-primary-subtle p-5">
+              <div className="text-center">
+                  <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"/>
+              </div>
+              <div className="text-center">
+                  <p className='fs-3'>Kiran V</p>
+                  <p className='fs-5'>Vishnu Institute of Computer Education and Technology</p>
+                  <p className='fs-5'>Bhimavaram</p>
+              </div>
+              <div className="text-center">
+                  <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"/>
+              </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
